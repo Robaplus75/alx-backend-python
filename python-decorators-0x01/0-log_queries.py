d38f7b6a63bf4d6b0b73acabeb169652f94630f4
robel <robelwon75@gmail.com>
2024-12-10 20:14:45 +0300

    log query

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,24 @@
+import sqlite3
+import functools
+
+def log_queries(orig_func):
+    @functools.wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        from datetime import datetime
+        result = orig_func(*args, **kwargs)
+        print(result)
+        print("Completed at: {}".format(datetime.now()))
+        return result
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+users = fetch_all_users(query="SELECT * FROM users")
---
+++ python-decorators-0x01/README.md
@@ -0,0 +1,1 @@
+Pyton decorators Project
