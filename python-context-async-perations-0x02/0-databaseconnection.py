5de42d336f1bc1781de80be9e97ecdee69b04500
robel <robelwon75@gmail.com>
2024-12-10 23:00:47 +0300

    database connection

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import mysql.connector
+
+class DatabaseConnection:
+    def __init__(self, user, host, password):
+        self.user = user
+        self.host = host
+        self.password = password
+
+    def __enter__(self):
+        db_conn = mysql.connector.connect(user=self.user, host=self.host, password=self.password)
+        return db_conn
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return False
+
+
+with DatabaseConnection('robel', 'localhost', '123') as db:
+    db_cursor = db.cursor()
+    db_cursor.execute("use airbnb_db;")
+    db_cursor.execute("SELECT * FROM User;")
+
+    rows = db_cursor.fetchall()
+
+    for row in rows:
+        print(row)
