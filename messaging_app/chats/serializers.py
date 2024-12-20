ef84e6760db9cf3c40336b689aa5714feebf9c2e
robel <robelwon75@gmail.com>
2024-12-20 21:28:03 +0300

    serializers setup 2

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,17 +1,23 @@
 from django_restframework import serializers
-from . import models
+from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
-		model = models.User
+		model = User
 		fields = '__all__'
 
 class MessageSerializer(serializers.ModelSerializer):
 	class Meta:
-		model = models.Message
+		model = Message
 		fields = '__all__'
 
 class ConversationSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = models.Conversation
-		fields = '__all__'
+	messages = MessageSerializer(many=True, read_only=True)  # Assuming a related_name for messages
+    message_count = serializers.SerializerMethodField()  # Example of using SerializerMethodField
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'messages', 'start_time', 'message_count']
+
+    def get_message_count(self, obj):
+        return obj.messages.count()  # Assuming messages is a related name for the Message model
