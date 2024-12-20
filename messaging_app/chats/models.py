de98635bffdeef3214f986bb1b079f2b11f4e030
robel <robelwon75@gmail.com>
2024-12-20 20:56:42 +0300

    models setup(added ondelete on foreign keys)

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,6 +7,9 @@ class User(AbstractUser):
 	role_choices = ('guest', 'host', 'admin')
 
 	user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_index=True)
+	first_name = models.CharField(max_length=255, null=False, blank=False)
+	last_name = models.CharField(max_length=255, null=False, blank=False)
+	email = models.EmailField(unique=True, null=False, blank=False)
 	password_hash = models.CharField(null=False, blank=False)
 	phone_number = models.CharField(null=True)
 	role = models.CharField(max_length=15, choices=role_choices, null=False, blank=False, default='guest')
@@ -17,21 +20,15 @@ class User(AbstractUser):
 
 class Message(models.Model):
 	message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_index=True)
-	sender_id = models.ForeignKey(User)
+	sender_id = models.ForeignKey(User, on_delete=False)
 	message_body = models.TextField(null=False, blank=False)
 	sent_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.message_body
 
-
-# Conversation
-#     conversation_id (Primary Key, UUID, Indexed)
-#     participants_id (Foreign Key, references User(user_id)
-#     created_at (TIMESTAMP, DEFAULT CURRENT_TIMESTAMP)
-
 class Conversation(models.Model):
 	conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_index=True)
-	participants_id = models.ForeignKey(User)
+	participants_id = models.ForeignKey(User, on_delete=False)
 	created_at = models.DateTimeField(auto_now_add=True)
 
