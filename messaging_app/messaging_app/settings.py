4b27e4ef454e69b295283d425b14863468cabbe5
robel <robelwon75@gmail.com>
2025-02-23 19:55:57 +0300

    requirements update

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qr)9^snafeljt&418jq$h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
